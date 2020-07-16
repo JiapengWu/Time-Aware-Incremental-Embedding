880b48f4b1e4168cb3aabda27b1ff6c85fb9ce5c
Jiapeng Wu <j84165602@china.huawei.com>
2020-07-16 13:48:13 -0400

    implemented metric printing file and jupyter notebook for visualization

---
+++ utils/print_metrics.py
@@ -0,0 +1,20 @@
+import pickle
+import sys
+import pdb
+import os
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    fname = os.path.join(sys.argv[1], "metrics-per-snapshot.pt")
+    with open(fname, 'rb') as fp:
+        metrics = pickle.load(fp)
+
+    for k, vs in metrics.items():
+        print(k)
+        # pdb.set_trace()
+        if type(vs) == dict:
+            for v in vs.values():
+                print(v)
+        else:
+            print(vs)
+        print()
